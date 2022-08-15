2c91b446554e60a902fc730346e876d505b2f458
co-labo-hub <co.labo.hub@gmail.com>
2022-08-15 15:21:18 +0900

    件名のプレースホルダも置換

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,5 +1,75 @@
 ((PLUGIN_ID) => {
-  'use strict';
+	'use strict';
+
+	const makeDialog = (id, html) => {
+		document.getElementById(id)?.remove();
+		const tpl = document.createElement('template');
+		tpl.innerHTML = /* html */`
+			<dialog
+				id="${id}"
+				style="border: solid 1px silver"
+			>
+				<span
+					class="material-symbols-outlined"
+					style="${[
+						'display: inline-flex',
+						'vertical-align: middle',
+						'line-height: 2em'
+					].join(';')}"
+				>
+					drafts
+				</span>
+				<a
+					onclick="document.getElementById('${id}').close()"
+					style="${[
+						'float: right',
+						'color: gray',
+						'font-size: 2em'
+					].join(';')}"
+				>
+					×
+				</a>
+				<br />
+				${html}
+			</dialog>
+		`;
+		document.body.appendChild(tpl.content);
+		document.querySelector(`#${id}`).showModal();
+		tpl.remove();	
+	};
+	const replacePlaceholder = (str, thisAppRecord) => {
+		let ret = str;
+		Object.entries(thisAppRecord).forEach(([fieldCode, value]) => {
+			let v = value.value;
+			if (Array.isArray(v)) v = v[0];
+			if (v instanceof(Object)) v = v.name || v.value;
+			ret = ret.replace(new RegExp(`{{\\s*${fieldCode}\\s*}}`, 'g'), v);
+		});
+		return ret.replace(/\n/g, "\r\n").replace(/\r+\n/g, "\r\n");
+	};
+	const getTemplates = (tplRecords, thisAppRecord, config) => {
+		const templates = [];
+		tplRecords.forEach(t => {
+			const el = [];
+			if (t[config.ccFieldCode]?.value) el.push(`cc=${t[config.ccFieldCode].value}`);
+			if (t[config.bccFieldCode]?.value) el.push(`bcc=${t[config.bccFieldCode].value}`);
+			if (t[config.subjectFieldCode]?.value) {
+				el.push(`subject=${encodeURIComponent(
+					replacePlaceholder(t[config.subjectFieldCode].value, thisAppRecord)
+				)}`);
+			}
+			if (t[config.bodyFieldCode]?.value) {
+				el.push(`body=${encodeURIComponent(
+					replacePlaceholder(t[config.bodyFieldCode].value, thisAppRecord)
+				)}`);
+			}
+			templates.push({
+				name: t[config.templateNameFieldCode].value,
+				params: el
+			});
+		});
+		return templates;	
+	};
 
 	kintone.events.on(['app.record.detail.show',
 						'app.record.index.show',
@@ -23,87 +93,46 @@
 		};
 		return kintone.api(kintone.api.url('/k/v1/records', true), 'GET', params)
 		.then(({records}) => {
-			if (!records.length) return;	// no records in Email template app
+			if (!records.length) return event;	// no records in Email template app
 
 			const emailToRecord = new Map();
 			(event.records || [event.record]).forEach(r => {
 				emailToRecord.set(r[config.emailFieldCode].value, r);
 			});
+
 			anchors.forEach(a => {
-				const r = emailToRecord.get(a.textContent?.trim() || '');
-				if (!r) return;
-
-				const getTemplates = tplRecords => {
-					const templates = [];
-					tplRecords.forEach(t => {
-						const el = [];
-						if (t[config.ccFieldCode]?.value) el.push(`cc=${t[config.ccFieldCode].value}`);
-						if (t[config.bccFieldCode]?.value) el.push(`bcc=${t[config.bccFieldCode].value}`);
-						if (t[config.subjectFieldCode]?.value) el.push(`subject=${encodeURIComponent(t[config.subjectFieldCode].value)}`);
-						if (t[config.bodyFieldCode]?.value) {
-							let body = t[config.bodyFieldCode].value;
-							Object.entries(r).forEach(([fieldCode, value]) => {
-								let v = value.value;
-								if (Array.isArray(v)) v = v[0];
-								if (v instanceof(Object)) v = v.name || v.value;
-								body = body.replace(new RegExp(`{{\\s*${fieldCode}\\s*}}`, 'g'), v);
-							});
-							body = body.replace(/\n/g, "\r\n").replace(/\r+\n/g, "\r\n");
-							el.push(`body=${encodeURIComponent(body)}`);
-						}
-						templates.push({name: t[config.templateNameFieldCode].value, params: el});
-					});
-					return templates;	
-				};
-
-				const makeDialog = (id, html) => {
-					document.getElementById(id)?.remove();
-					const tpl = document.createElement('template');
-					tpl.innerHTML = /* html */`
-						<dialog
-							id="${id}"
-							style="border: solid 1px silver;"
-						>
-							<span
-								class="material-symbols-outlined"
-								style="display: inline-flex; vertical-align: middle; line-height: 2em"
-							>
-								drafts
-							</span>
-							<a
-								onclick="document.getElementById('${id}').close()"
-								style="float: right; color: gray; font-size: 2em"
-							>
-								×
-							</a>
-							<br />
-							<br />
-							${html}
-						</dialog>
-					`;
-					document.body.appendChild(tpl.content);
-					document.querySelector(`#${id}`).showModal();
-					tpl.remove();	
-				};
+				const thisAppRecord = emailToRecord.get(a.textContent?.trim() || '');
+				if (!thisAppRecord) return;
 
 				a.onclick = (aEvent) => {
 					aEvent.preventDefault();
 
+					const dialogId = 'email-template-dialog';
 					const html = /* html */`
 					<ul style="padding-inline-start: 1em">
-						${getTemplates(records).map((t) => /* html */`
+						${getTemplates(records, thisAppRecord, config).map(t => /* html */`
 							<li style="margin: 0.5em 0">
-								<a href="${a.href}?${t.params.join('&')}" target="_blank">
+								<a
+									href="${a.href}?${t.params.join('&')}"
+									target="_blank"
+									onclick="document.getElementById('${dialogId}').close()"
+								>
 									${t.name}
 								</a>
 							</li>`
 						).join('')}
 					</ul>`;
-					makeDialog('email-template-dialog', html);
+					makeDialog(dialogId, html);
 				};
 			});
+			return event;
 		})
-		//.catch(console.error);
+		.catch((err) => {
+			console.error(err);
+			//event.error = err.message;	doesn't work.
+			//return event;
+			alert(err.message);
+		});
 	});
 
 })(kintone.$PLUGIN_ID);
